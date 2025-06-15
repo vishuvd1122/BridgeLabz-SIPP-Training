ec67e6378fb802cc0befbe81b9a4734a741b33d4
vishuvd1122 <vishu.diwakar_cs22@gla.ac.in>
2025-06-15 18:15:56 +0530

    [VIshu] Add . Print  odd and even numbers

---
+++ Control_Flow/OddEvenPrinter.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class OddEvenPrinter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a natural number: ");
+        int number = scanner.nextInt();
+
+        if (number <= 0) {
+            System.out.println("Please enter a valid natural number (> 0).");
+        } else {
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is Even");
+                } else {
+                    System.out.println(i + " is Odd");
+                }
+            }
+        }
+
+        scanner.close();
+    }
+}
