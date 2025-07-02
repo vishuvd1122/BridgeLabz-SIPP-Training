00842914590ffc6e9c9e9eef61c485970714646e
vishuvd1122 <vishu.diwakar_cs22@gla.ac.in>
2025-07-02 08:27:00 +0530

    [Vishu] Add . Hotel Booking code

---
+++ OOPS/Constructor/Basic Calculator/HotelBooking.java
@@ -0,0 +1,26 @@
+class HotelBooking {
+    String guestName;
+    String roomType;
+    int nights;
+
+    // Default constructor
+    HotelBooking() {
+        this.guestName = "Unknown";
+        this.roomType = "Standard";
+        this.nights = 1;
+    }
+
+    // Parameterized constructor
+    HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    // Copy constructor
+    HotelBooking(HotelBooking booking) {
+        this.guestName = booking.guestName;
+        this.roomType = booking.roomType;
+        this.nights = booking.nights;
+    }
+}
