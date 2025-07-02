ec6fb36a44b7ff6da25352dc7d1eadbe68c29a51
vishuvd1122 <vishu.diwakar_cs22@gla.ac.in>
2025-07-02 09:20:23 +0530

    [Vishu] Add Book

---
+++ OOPS/Java_Keywords/Book.java
@@ -0,0 +1,23 @@
+class Book {
+    static String libraryName = "City Library";
+    final String isbn;
+    String title, author;
+
+    Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    static void displayLibraryName() {
+        System.out.println("Library: " + libraryName);
+    }
+
+    void displayBookDetails() {
+        if (this instanceof Book) {
+            System.out.println("Title: " + title);
+            System.out.println("Author: " + author);
+            System.out.println("ISBN: " + isbn);
+        }
+    }
+}
