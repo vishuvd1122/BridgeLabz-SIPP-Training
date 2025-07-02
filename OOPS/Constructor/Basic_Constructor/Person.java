abf2a057cd5116ec9b39be7166c0d50422253e62
vishuvd1122 <vishu.diwakar_cs22@gla.ac.in>
2025-07-02 08:59:31 +0530

    [Vishu] Add . Person code

---
+++ OOPS/Constructor/Basic_Constructor/Person.java
@@ -0,0 +1,15 @@
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Copy constructor
+    Person(Person p) {
+        this.name = p.name;
+        this.age = p.age;
+    }
+}
