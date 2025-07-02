0a822ce23a02d7b2f1762d3f2028ccdb15a8109a
vishuvd1122 <vishu.diwakar_cs22@gla.ac.in>
2025-07-02 09:15:13 +0530

    [Vishu] Add Product

---
+++ OOPS/Constructor/Constructor_2/Product.java
@@ -0,0 +1,24 @@
+public class Product {
+    // Instance variables
+    private String productName;
+    private double price;
+
+    // Class variable 
+    private static int totalProducts = 0;
+
+    // Constructor
+    public Product(String productName, double price) {
+        this.productName = productName;
+        this.price = price;
+        totalProducts++; // Increment total when a new product is created
+    }
+
+    public void displayProductDetails() {
+        System.out.println("Product Name: " + productName);
+        System.out.println("Price: ₹" + price);
+    }
+
+    public static void displayTotalProducts() {
+        System.out.println("Total products created: " + totalProducts);
+    }
+}
