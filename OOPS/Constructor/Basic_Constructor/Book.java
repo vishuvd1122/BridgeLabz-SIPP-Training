6e0c2d48da71f4494a00cd77dd0ffa179488575a
vishuvd1122 <vishu.diwakar_cs22@gla.ac.in>
2025-07-02 09:00:07 +0530

    [Vishu] Add . Code for Book

---
+++ OOPS/Constructor/Basic_Constructor/Book.java
@@ -0,0 +1,20 @@
+
+class Book {
+    String title;
+    String author;
+    double price;
+
+    // Default constructor
+    Book() {
+        this.title = "Unknown";
+        this.author = "Unknown";
+        this.price = 0.0;
+    }
+
+    // Parameterized constructor
+    Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+}
