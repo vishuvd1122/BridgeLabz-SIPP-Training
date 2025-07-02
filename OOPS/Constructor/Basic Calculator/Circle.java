5b18bd2ee1c708d56fcdf547b179c6b0fb0a00ad
vishuvd1122 <vishu.diwakar_cs22@gla.ac.in>
2025-07-02 08:25:52 +0530

    [Vishu] Add . Circle code

---
+++ OOPS/Constructor/Basic Calculator/Circle.java
@@ -0,0 +1,14 @@
+
+    class Circle {
+    double radius;
+
+    // Default constructor
+    Circle() {
+        this(1.0);  // calling parameterized constructor
+    }
+
+    // Parameterized constructor
+    Circle(double radius) {
+        this.radius = radius;
+    }
+}
